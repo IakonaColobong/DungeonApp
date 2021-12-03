d6ce265d3647c5fb7b01287e80dcf35cafbcecfc
Jason Colobong <jasoncolobong@outlook.com>
2021-12-03 14:13:46 -0600

    Added Classes

---
+++ CharacterClasses/CharacterClasses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+
+{
+    public enum CharacterClasses
+    {
+        Warrior,
+        Mage,
+        Thief,
+        Ranger,
+        Priest
+     
+
+
+    }//end enum
+}//end namespace
---
+++ CharacterClasses/Rooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterClasses
+{
+    //will contain the descriptions for random rooms
+    public class Rooms
+    {
+
+    }
+}
